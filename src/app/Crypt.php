eac39fe757d895bfca657004ba92ba46bcbbeb99
whdabai <1733886703@qq.com>
2021-04-11 11:36:18 +0800

    Signed-off-by: whdabai <1733886703@qq.com>

--- src/app/Crypt.php
+++ src/app/Crypt.php
@@ -15,7 +15,7 @@ final class Crypt
      * @param $appid string 小程序的appid
      * @param $sessionKey string 用户在小程序登录后获取的会话密钥
      */
-    public function __construct($appid, $sessionKey)
+    public function __construct(string $appid, string $sessionKey)
     {
         $this->appid = $appid;
         $this->sessionKey = $sessionKey;
@@ -23,31 +23,28 @@ final class Crypt
 
     /**
      * 检验数据的真实性，并且获取解密后的明文.
-     * @param $encryptedData string 加密的用户数据
-     * @param $iv string 与用户数据一同返回的初始向量
+     * @param string $encryptedData string 加密的用户数据
+     * @param string $iv string 与用户数据一同返回的初始向量
      * @param $data
      *
      * @return int 成功0，失败返回对应的错误码
      */
-    public function decryptData($encryptedData, $iv, &$data)
+    public function decryptData(string $encryptedData, string $iv, &$data)
     {
-        if (strlen($this->sessionKey) != 24) {
-            return 'encodingAesKey 非法';
-        }
+        if (empty($this->sessionKey)) return 'encodingAesKey 不能为空';
+        if (strlen($this->sessionKey) !== 24) return 'encodingAesKey 非法，须为24位长';
+        if (strlen($iv) !== 24) return 'iv 非法，须为24位长';
         $aesKey = base64_decode($this->sessionKey);
-
-        if (strlen($iv) != 24) return 'iv 非法';
-
         $aesIV = base64_decode($iv);
-
         $aesCipher = base64_decode($encryptedData);
 
         $result = openssl_decrypt($aesCipher, "AES-128-CBC", $aesKey, 1, $aesIV);
+        if ($result === false) return '解析失败';
 
-        $dataObj = json_decode($result);
-        if ($dataObj === NULL) return '解密buffer非法';
-        if ($dataObj->watermark->appid != $this->appid) return '解密appid非法';
-        $data = $result;
+        $data = json_decode($result, true);
+        if ($data === NULL) return '解密buffer非法';
+        if (($data['watermark']['appid'] ?? '') != $this->appid) return '解析appid非法';
+
         return true;
     }
 
