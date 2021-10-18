61bd0e09a25ffc20941bbde29a350348cbe8b955
LaoCC <fazo@qq.com>
2021-10-18 12:38:17 +0800

    Signed-off-by: LaoCC <fazo@qq.com>

--- src/items/Fans.php
+++ src/items/Fans.php
@@ -32,7 +32,7 @@ final class Fans extends Base
      * @throws Exception
      * https://mp.weixin.qq.com/wiki?t=resource/res_main&id=mp1421140840
      */
-    public function load_AllFans(callable $call, string $next = '')
+    public function load_AllFans(callable $call, string $next = ''): int
     {
         $apiBase = "/cgi-bin/user/get?access_token={access_token}&next_openid=%s";
         $sumFans = 0;
@@ -63,29 +63,24 @@ final class Fans extends Base
     }
 
     /**
-     * 读取OpenID
-     * @param bool|false $getWayOpenID 仅返回OpenID
-     * @return array|string
-     * http://mp.weixin.qq.com/wiki/17/c0f37d5704f0b64713d5d2c37b468d75.html
-     * https://mp.weixin.qq.com/wiki?t=resource/res_main&id=mp1421140842
+     * 读取OpenID前跳转
+     *
+     * @param array $option
      */
-    public function get_OpenID_URL(bool $getWayOpenID = false)
+    public function redirect(array $option)
     {
-//        $scope = $getUserInfo ? "snsapi_userinfo" : 'snsapi_base';
-        $state = md5(date('Ymd') . $this->AppID);    //state标识 重定向后会带上state参数，开发者可以填写a-zA-Z0-9的参数值
-
-        //最后要跳回来的页面
-        $uri = _HTTP_ . getenv('HTTP_HOST') . getenv('REQUEST_URI');
-        $uri_base = urlencode(base64_encode($uri));
+        $backUrl = $option['url'] ?? null;  //最后要跳回来的页面
+        if (is_null($backUrl)) $backUrl = _HTTP_ . getenv('HTTP_HOST') . getenv('REQUEST_URI');
+        $uri_base = urlencode(base64_encode($backUrl));
 
         $param = [];
         $param['appid'] = $this->AppID;
         $param['redirect_uri'] = '';
         $param['response_type'] = 'code';
-        $param['scope'] = 'snsapi_base';//"snsapi_userinfo" : 'snsapi_base'
-        $param['state'] = $state;
+        $param['scope'] = ($option['scope'] ?? 'snsapi_base');//"snsapi_userinfo" : 'snsapi_base'
+        $param['state'] = $this->sign_url();
 
-        $isPay = $getWayOpenID ? 1 : 0;
+        $isPay = ($option['pay'] ?? 0) ? 1 : 0;
 
         $time = getenv('REQUEST_TIME_FLOAT') . '.' . mt_rand();
         $sign = md5($this->AppID . $time . 'OPENID');
@@ -96,58 +91,45 @@ final class Fans extends Base
             $url = "{$this->mpp['domain']}/login/openid/{$this->AppID}/{$isPay}/{$uri_base}/{$time}/{$sign}/";
         }
 
-        $param['redirect_uri'] = $url;
+//        $param['redirect_uri'] = $url;
+        $param['redirect_uri'] = $backUrl;
         $args = http_build_query($param);
         $api = "https://open.weixin.qq.com/connect/oauth2/authorize?{$args}#wechat_redirect";
 
-        $this->debug(['appURL' => $uri, 'redirectURL' => $url, 'redirectAPI' => $api, 'param' => $param]);
-        return $api;
+        $this->debug(['appURL' => $backUrl, 'redirectURL' => $url, 'redirectAPI' => $api, 'param' => $param]);
+        header('Expires: ' . gmdate('D, d M Y H:i:s', time() - 1) . ' GMT');
+        header("Cache-Control: no-cache");
+        header("Pragma: no-cache");
+        header("Location: {$api}");
+        fastcgi_finish_request();
+        exit;
     }
 
     /**
-     * @param bool $isPay
-     * @param string|null $backUrl
+     * @param string|null $state
+     * @return bool|string
+     */
+    private function sign_url(string $state = null)
+    {
+        $token = md5(date('Ymd') . $this->AppID);
+        if (is_null($state)) return $token;
+
+        return $token === $state;
+    }
+
+    /**
+     * @param array $option
      * @return array|string
      * @throws Exception
      */
-    public function load_OpenID(bool $isPay = false, string $backUrl = null)
+    public function load_OpenID(array $option)
     {
-        //state标识 重定向后会带上state参数，开发者可以填写a-zA-Z0-9的参数值
-        $state = md5(date('Ymd') . $this->AppID);
+        if (!isset($_GET['code']) or !isset($_GET['state'])) $this->redirect($option);
 
-        if (!isset($_GET['code']) or !isset($_GET['state'])) {
+        if (!$this->sign_url($_GET['state'])) return "state与传入值不一致";
 
-            //最后要跳回来的页面，也就是当前页面
-            if (is_null($backUrl)) $backUrl = _HTTP_ . getenv('HTTP_HOST') . getenv('REQUEST_URI');
-            $uri_base = urlencode(base64_encode($backUrl));
-            $isPay = $isPay ? 1 : 0;
+        if ($this->Platform) return $this->Platform->loadOpenID();
 
-            $param = [];
-            $param['appid'] = $this->AppID;
-            $param['redirect_uri'] = '';
-            $param['response_type'] = 'code';
-            $param['scope'] = 'snsapi_base';//"snsapi_userinfo" : 'snsapi_base'
-            $param['state'] = $state;
-
-            if ($this->Platform) {
-                $param['component_appid'] = $this->Platform->PlatformAppID;
-                $url = "{$this->Platform->PlatformURL}/user/openid/{$this->AppID}/{$isPay}/{$uri_base}/{$state}/";
-            } else {
-                $url = "{$this->mpp['domain']}/login/openid/{$this->AppID}/{$isPay}/{$uri_base}/{$state}/";
-            }
-
-            $param['redirect_uri'] = $url;
-            $args = http_build_query($param);
-            $api = "https://open.weixin.qq.com/connect/oauth2/authorize?{$args}#wechat_redirect";
-            $this->debug(['backUrl' => $backUrl, 'redirectURL' => $url, 'redirectAPI' => $api, 'param' => $param]);
-            return $api;
-        }
-
-        /**
-         * 进行到这里，这是APP自身进行请求，对于第三方不是在这儿请求
-         * 第三方在PlatformURL/user/openid中直接请求，并且会带上openID返回
-         */
-        if ($_GET['state'] !== $state) return "state与传入值不一致";
         $param = [];
         $param['appid'] = $this->AppID;
         $param['secret'] = $this->mpp['secret'];
