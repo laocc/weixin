30adebd0cc28ec90bce50f68f2b3634bb3bb01c5
ddd <ddd@qqq.com>
2022-04-08 17:09:25 +0800

    Signed-off-by: ddd <ddd@qqq.com>

--- src/app/Scheme.php
+++ src/app/Scheme.php
@@ -3,13 +3,32 @@
 namespace esp\weiXin\app;
 
 use esp\weiXin\Base;
+use Exception;
 
 class Scheme extends Base
 {
+    /**
+     * @param array $jump
+     * @return array|string
+     * @throws Exception
+     *
+     * https://developers.weixin.qq.com/miniprogram/dev/api-backend/open-api/url-scheme/urlscheme.generate.html
+     */
     public function create(array $jump)
     {
         $value = [];
-        $value['is_expire'] = false;
+        $value['is_expire'] = isset($jump['expire']);
+
+        if (isset($jump['expire'])) {
+            if ($jump['expire'] > 10000) {//这是时间戳
+                $value['expire_type'] = 0;
+                $value['expire_time'] = $jump['expire'];
+            } else {//间隔的天数
+                $value['expire_type'] = 1;
+                $value['expire_interval'] = $jump['expire'];
+            }
+        }
+
         $value['jump_wxa'] = [];
         $value['jump_wxa']['path'] = $jump['path'] ?? '';
         $value['jump_wxa']['query'] = $jump['query'] ?? '';
