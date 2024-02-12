4ca8ac575c80306fb6b69be8574e86e81c62f2e6
老船长 <fazo@qq.com>
2024-02-13 01:08:43 +0800

    init

--- src/app/Crypt.php
+++ src/app/Crypt.php
@@ -27,12 +27,12 @@ final class Crypt
      * @param string $iv string 与用户数据一同返回的初始向量
      * @param $data
      *
-     * @return int 成功0，失败返回对应的错误码
+     * @return bool|string 成功0，失败返回对应的错误码
      */
     public function decryptData(string $encryptedData, string $iv, &$data)
     {
-        if (empty($this->sessionKey)) return 'encodingAesKey 不能为空';
-        if (strlen($this->sessionKey) !== 24) return 'encodingAesKey 非法，须为24位长';
+        if (empty($this->sessionKey)) return 'sessionKey 不能为空';
+        if (strlen($this->sessionKey) !== 24) return 'sessionKey 非法，须为24位长';
         if (strlen($iv) !== 24) return 'iv 非法，须为24位长';
         $aesKey = base64_decode($this->sessionKey);
         $aesIV = base64_decode($iv);
@@ -43,7 +43,10 @@ final class Crypt
 
         $data = json_decode($result, true);
         if ($data === NULL) return '解密buffer非法';
-        if (($data['watermark']['appid'] ?? '') != $this->appid) return '解析appid非法';
+        $appid = $data['watermark']['appid'] ?? '';
+        if ($appid && ($appid != $this->appid)) {
+            return "解析appid不符：{$appid}!=={$this->appid}";
+        }
 
         return true;
     }
